7ec3acaa806214fa500b999a8d9a82ce1d2dd1a8
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-02-15 18:30:35 +0530

    JWT TOKEN ADDED

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 import express from "express";
 import UserSchema from "../models/User.js";
 import { body, validationResult } from "express-validator";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const authRouter = express.Router();
+
+const JWT_SECRET = "AAYUSH_IS_THE_BEST";
 
 //Create a User using POST "/api/auth/createuser" . Doesn't require Auth No Login Required
 
@@ -22,20 +25,33 @@ authRouter.post(
       return res.status(400).json({ errors: errors.array() });
     }
     try {
+      //Check if user exists already
       let user = await UserSchema.findOne({ email: req.body.email });
       if (user) {
-        // Duplicate key error
         return res.status(400).json({
           error: "Email already exists",
         });
       } else {
+        //If not then create user
+
+        const salt = (await bcrypt.genSalt(10)).toString();
+        const secPass = (await bcrypt.hash(req.body.password, salt)).toString();
+
         user = await UserSchema.create({
           name: req.body.name,
           email: req.body.email,
-          password: req.body.password,
+          password: secPass,
         });
+        //Show what was sent as response
+        const data = {
+          user: {
+            id: user.id,
+          },
+        };
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.json({ authToken: authToken });
       }
-      res.json({ Message: "User Created" });
     } catch (error) {
       return res.status(500).json({
         error: "Server error",
