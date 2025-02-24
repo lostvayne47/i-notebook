6ac6449eb242d7c1fe7fe33e6cdd1e6dcf9d3bcd
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-02-24 18:17:37 +0530

    Empty Notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -17,6 +17,11 @@ export default function Notes() {
       <AddNote />
       <div className="row my-3">
         <h2>Your Notes</h2>
+        {notes.length === 0 && (
+          <div className="container d-flex justify-content-center align-items-center">
+            <h2>No notes to display, add a Note!</h2>
+          </div>
+        )}
         {notes.map((note) => {
           return <NoteItem key={note._id} note={note}></NoteItem>;
         })}
