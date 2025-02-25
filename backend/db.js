11d1870308c6e7fc8e17916a5e623cb2c949a256
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-02-25 20:40:58 +0530

    MongoDB Atlas Connected

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-
-const mongoURI = "mongodb://localhost:27017/i-notebook";
-
+// const mongoURI = "mongodb://localhost:27017/i-notebook";
+const mongoURI =
+  "mongodb+srv://aayushkamtikar:NUTqSkxFgu8PGgDO@myfirstcluster.m6a26.mongodb.net/i-notebook";
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
   console.log("Connected to mongo successfully");
